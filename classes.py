7450e8fcc8141a4704290cb316697ee7f2efbe71
NateMo <natemondero@gmail.com>
2024-10-25 20:58:54 -0400

    Commit Message (I forgot to commit earlier so theres a bunch of new stuff and i forgot what i modified)

--- classes.py
+++ classes.py
@@ -39,13 +39,13 @@ class Dice:
         return self.dice1Val, self.dice2Val
     
     #Draw the dice values onto the top right of the screen at the specified rectangles (We can change the positions if we need to depending on screen size)
-    def draw(self, surface: pygame.Surface) -> None:
-        surface.blit(self.dice1Img, self.dice1Rect)
-        surface.blit(self.dice2Img, self.dice2Rect)
+    def draw(self, screen: pygame.Surface) -> None:
+        screen.blit(self.dice1Img, self.dice1Rect)
+        screen.blit(self.dice2Img, self.dice2Rect)
 
 class Player:
     #Constructor (Only playerName and token are required. All other values have default values for creating players at the start of the game)
-    def __init__(self, name: str, token: PlayerToken, balance: int = 1500, position: int = 0, properties: Optional[List[Property]] = None, cards: Optional[List[Card]] = None, isInJail: bool = False, consecutiveDoubles: int = 0, turnsLeftInJail: int = 0, isBankrupt: bool = False):
+    def __init__(self, name: str, token: PlayerTokenImage, balance: int = 1500, position: int = 0, properties: Optional[List[Property]] = None, cards: Optional[List[Card]] = None, isInJail: bool = False, consecutiveDoubles: int = 0, turnsLeftInJail: int = 0, isBankrupt: bool = False):
         self.playerName = name
         self.token = token
         self.playerBalance = balance
@@ -57,41 +57,78 @@ class Player:
         self.turnsLeftInJail = turnsLeftInJail
         self.isBankrupt = isBankrupt
 
-    #Add an amount to player balance
-    def addBalance(self, balanceToAdd):
-        self.playerBalance += balanceToAdd
-
-    #Remove an amount from player balance
-    def removeBalance(self, balanceToRemove):
-        self.playerBalance -= balanceToRemove
-        if self.playerBalance < 0:
-            self.isBankrupt = True
-
     #Add a property to player's property list
-    def addProperty(self, propertyToAdd):
-        self.properties += propertyToAdd
+    def addProperty(self, propertyToAdd: Property) -> None:
+        self.propertyList += propertyToAdd
 
     #Remove a property from player's property list
-    def removeProperty(self, propertyToRemove):
-        if propertyToRemove not in self.properties:
+    def removeProperty(self, propertyToRemove: Property) -> None:
+        if propertyToRemove not in self.properties: #Raise exception if the property is not in the player's property list
             raise ValueError("Exception: Property to remove is not in player's property list.")
-        else: 
-            self.properties.remove(propertyToRemove)
+        self.propertyList.remove(propertyToRemove) #Remove the property from the player's property list
 
-    #Return the player's balance
-    def getBalance(self) -> int:
-        return self.playerBalance
+    #Add an amount to player balance
+    def addBalance(self, balanceToAdd: int) -> None:
+        if (balanceToAdd <= 0): #Raise an exception if invalid parameter
+            raise ValueError("Exception: Balance to add ({balanceToAdd}) is not greater than 0.")
+        self.playerBalance += balanceToAdd #Add specified amount to player balance
+
+    #Remove an amount from player balance
+    def removeBalance(self, balanceToRemove: int) -> None:
+        if (balanceToRemove <= 0): #Raise exception if invalid parameter
+            raise ValueError("Exception: Balance to remove ({balanceToRemove}) is not greater than 0.")
+
+        self.playerBalance -= balanceToRemove #Remove specified amount from player 
+        
+        ''' 
+        Note: I am thinking of moving this to the main so that we dont have to awkwardly pass the screen display to the removeBalance function to get the sellProperty function to work
+        if self.playerBalance < 0: #If player balance is less than 0, check if they can sell properties to avoid bankruptcy
+            totalPropertyValue = 0
+            for property in self.propertyList:
+                totalPropertyValue += property.sellValue
+            
+            if totalPropertyValue < balanceToRemove: #Player is bankrupt if they cannot sell enough value to get out of negative balance
+                self.isBankrupt = True
+            else: #Make the player sell property until they have enough money to stay out of negative
+                while self.playerBalance < balanceToRemove:
+                    self.sellProperty(screen)
+        '''
+    
+    def sellProperty(self, screen: pygame.Surface) -> None:
+        if self.propertyList.count() == 0:
+            raise ValueError("Exception: No properties to sell.")
+
+        '''
+        Create "Sell Property" Surface to display as a title for sell property screen
+        Create button objects (Surfaces) that will display properties and their sell values
+        Map each button to the properties in the player's property list
+        Create an array of Property that represents properties selected to sell
+        Create button objects to Confirm the sell or cancel the sell
+        Draw over the entire Display Surface with the background color then draw on the new button objects
+        Event loop looking for player clicks:
+            if player clicks a property button:
+                if the property for that button IS NOT in the properties to sell array (the button is UNCLICKED):
+                    add the property at that button to properties to sell array
+                    Update the button to look like it has been selected (probably change the Surface color)
+                if the property for that button IS in the properties to sell array:
+                    Remove the property at that button from properties to sell array
+                    Update the button to look like it has not been selected (probably change the Surface color back to default)
+            if player clicks Sell Properties:
+                For each property in the properties to sell array:
+                    Add property sell value to player balance
+                    Remove the property from player property array
+        '''
 
     '''
     I am thinking we simply just use this function to move the player by a specified amount (move amount) or move directly to a certain spot (jumpToTile) and a bool to specify if the jumpToTile should allow passing go
     ex. Move via dice roll (totalDiceVal = 6): 
-        player1.movePlayer(moveAmount = totaLDiceVal)
+        player1.movePlayer(moveAmount = totalDiceVal)
     ex. Pass directly to Go tile (index of zero)
         player1.movePlayer(jumpToTile = 0, passGoViable = True)
     ex. Go directly to jail, do not collect passing go money
         player1.movePlayer(jumpToTile = 10, passGoViable = False)
     '''
-    def movePlayer(self, moveAmount = None, jumpToTile = None, passGoViable = None):
+    def movePlayer(self, moveAmount: Optional[int] = None, jumpToTile: Optional[int] = None, passGoViable: Optional[bool] = None) -> None:
         #Raise exception if parameters are not provided (this function requires one of the parameters to be defined)
         if moveAmount == None and jumpToTile == None:
             raise ValueError("Exception: Function must have at least one parameter of the following parameters:\n(moveAmount = combined dice roll, jumpToTile = index of tile to jump to)")
@@ -116,6 +153,17 @@ class Player:
             if initialPosition > self.playerPosition and passGoViable:
                 self.playerBalance += 200
 
-        
-class PlayerToken:
-    pass
+
+class Board:
+    def __init__(self, tileArray: List[Tile], turnOrder: List[Player], eventCardDeck: List[Card], turnNumber: int):
+        self.tileArray = tileArray
+        self.turnOrder = turnOrder
+        self.evenCardDeck = eventCardDeck
+        self.turnNumber = turnNumber
+
+    def drawEvent(self, drawPlayer: Player) -> None:
+        pass
+    
+    def removePlayer(playerToRemove: Player) -> None:
+        pass
+
