f799365a091ccbd74f110d760dc5667b81b38ead
HunterJV2004 <68832557+HunterJV2004@users.noreply.github.com>
2024-11-05 01:38:21 +0000

    Added menu functionality

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import random
 os.environ['SDL_AUDIODRIVER'] = 'dsp'
 from classes import Dice, Event, Player, Tile
+from menu import Menu
 
 
 
@@ -19,19 +20,22 @@ clock = pygame.time.Clock()
 #Initialize game objects
 dice = Dice()
 card_popup = Event()
+menu = Menu(screen)
 
 
 
 
-token = PlayerTokenImage()
-player = Player(1, "Nate", token)
+#token = PlayerTokenImage()
+#player = Player(1, "Nate", token)
 running = True
+
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+        else:
+            menu.handle_event(event) 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RETURN:
                 rollRes = dice.roll()            
@@ -39,14 +43,17 @@ while running:
                     print("Doubles!")
             if event.key == pygame.K_SPACE:
                 card_popup.show_event_message(random.randint(1, 32))
-        
+      
         
         
 
-    screen.fill(background_color)     
-    dice.draw(screen) #draw dice onto the screen
-    card_popup.draw(screen) #draw the popup box on the screen
-    player.drawScore(screen)
+    screen.fill(background_color)   
+    
+    menu.draw() 
+    if(not menu.checkMenu()):
+        dice.draw(screen) #draw dice onto the screen
+        card_popup.draw(screen) #draw the popup box on the screen
+        #player.drawScore(screen)
 
     pygame.display.update() #update the display
     clock.tick(60) #one while loop 60 times per second
